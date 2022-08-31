a9a62c880f63711a3981367f0cb1db3bb17e85df
Valentyn Diduryk <stranger@strngr.name>
2022-09-01 00:36:49 +0300

    install docker-compose

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -8,6 +8,16 @@ DNS_ID=$(aws ssm get-parameter --name DNS_ID | jq -r .Parameter.Value)
 INSTANCE_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 CURRENT_IP=$(dig +short $DNS_RECORD)
 
+###
+
+if ! which docker-compose; then
+    echo "Installing docker-compose"
+    wget -c https://desktop.docker.com/linux/main/amd64/docker-desktop-4.11.1-amd64.deb
+    dpkg -i docker-desktop-4.11.1-amd64.deb
+fi
+
+###
+
 if [ "x$CURRENT_IP" == "x$INSTANCE_IP" ]; then
         echo "already valid"
         exit 0
@@ -22,3 +32,4 @@ echo "Current IP: $CURRENT_IP"
 curl $API_ENDPOINT \
         -H "Authorization: $API_TOKEN" \
         -d "subdomain_id=$DNS_ID&data=$INSTANCE_IP"
+
